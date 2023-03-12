045b3939a46aad1270923780763f377747429a88
Yhq00 <1193086708@qq.com>
2023-03-12 19:05:00 +0800

    3/12

--- src/App.js
+++ src/App.js
@@ -1,32 +1,145 @@
-import {Button} from 'antd';
 import 'antd/dist/reset.css'
 import './App.css';
+import { Carousel } from 'antd';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
+import React from 'react';
+import { Collapse } from 'antd';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+//折叠面板定义
+const { Panel } = Collapse;
+const text = `
+  A dog is a type of domesticated animal.
+  Known for its loyalty and faithfulness,
+  it can be found as a welcome guest in many households across the world.
+`;
 
-const App = () =>(
- <div className='App'>
-    <Button type='primary'></Button>
-  </div>
-)
+//走马灯定义
+const contentStyle = {
+  margin: 0,
+  height: '160px',
+  color: '#fff',
+  lineHeight: '160px',
+  textAlign: 'center',
+  background: '#364d79',
+};
 
+//layout布局定义
+const { Header, Content, Sider } = Layout;
+const items1 = ['1', '2', '3'].map((key) => ({
+  key,
+  label: `nav ${key}`,
+}));
+const items2 = [UserOutlined, LaptopOutlined, NotificationOutlined].map((icon, index) => {
+  const key = String(index + 1);
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: `subnav ${key}`,
+    children: new Array(4).fill(null).map((_, j) => {
+      const subKey = index * 4 + j + 1;
+      return {
+        key: subKey,
+        label: `option${subKey}`,
+      };
+    }),
+  };
+});
+
+const App = () => {
+//layout布局定义
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+
+//走马灯定义
+  const onChange = (currentSlide) => {
+    console.log(currentSlide);
+  };
+  
+//折叠面板定义
+  const onChange1 = (key) => {
+    console.log(key);
+  };
+
+  return (
+    <Layout>
+      <Header className="header">
+        <div className="logo" />
+        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']} items={items1} />
+      </Header>
+      <Layout>
+        <Sider
+          width={200}
+          style={{
+            background: colorBgContainer,
+          }}
+        >
+          <Menu
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            defaultOpenKeys={['sub1']}
+            style={{
+              height: '100%',
+              borderRight: 0,
+            }}
+            items={items2}
+          />
+        </Sider>
+        <Layout
+          style={{
+            padding: '0 24px 24px',
+          }}
+        >
+          <Breadcrumb
+            style={{
+              margin: '16px 0',
+            }}
+          >
+            <Breadcrumb.Item>Home</Breadcrumb.Item>
+            <Breadcrumb.Item>List</Breadcrumb.Item>
+            <Breadcrumb.Item>App</Breadcrumb.Item>
+          </Breadcrumb>
+          <Content
+            style={{
+              padding: 24,
+              margin: 0,
+              minHeight: 280,
+              background: colorBgContainer,
+            }}
+          >
+            {/* 走马灯 */}
+            <Carousel afterChange={onChange}>
+      <div>
+        <h3 style={contentStyle}>1</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>2</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>3</h3>
+      </div>
+      <div>
+        <h3 style={contentStyle}>4</h3>
+      </div>
+    </Carousel>
+
+    {/* 折叠面板 */}
+    <Collapse defaultActiveKey={['1']} onChange1={onChange1}>
+      <Panel header="This is panel header 1" key="1">
+        <p>{text}</p>
+      </Panel>
+      <Panel header="This is panel header 2" key="2">
+        <p>{text}</p>
+      </Panel>
+      <Panel header="This is panel header 3" key="3">
+        <p>{text}</p>
+      </Panel>
+    </Collapse>
+          </Content>
+        </Layout>
+      </Layout>
+    </Layout>
+  );
+};
 export default App;
