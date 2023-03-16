ff6a849c40373386dbff4920580cc6844a7e08fc
Yhq <1193086708@qq.com>
2023-03-16 16:46:05 +0800

    add two components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
 import 'antd/dist/reset.css'
 import './App.css';
-import { Carousel } from 'antd';
 import { Breadcrumb, Layout, Menu, theme } from 'antd';
 import { LaptopOutlined, NotificationOutlined, UserOutlined } from '@ant-design/icons';
 import React from 'react';
-import { Collapse } from 'antd';
-
-//折叠面板定义
-const { Panel } = Collapse;
-const text = `我是分支一的修改
-  A dog is a type of domesticated animal.
-  Known for its loyalty and faithfulness,
-  it can be found as a welcome guest in many households across the world.我是分支二的修改
-`;
-
-//走马灯定义
-const contentStyle = {
-  margin: 0,
-  height: '160px',
-  color: '#fff',
-  lineHeight: '160px',
-  textAlign: 'center',
-  background: '#364d79',
-};
+import MyFirstComponent from './components/MyFirstComponent';
+import MyTwoComponent from './components/MyTwoComponent';
 
 //layout布局定义
 const { Header, Content, Sider } = Layout;
@@ -52,15 +34,6 @@ const App = () => {
     token: { colorBgContainer },
   } = theme.useToken();
 
-//走马灯定义
-  const onChange = (currentSlide) => {
-    console.log(currentSlide);
-  };
-  
-//折叠面板定义
-  const onChange1 = (key) => {
-    console.log(key);
-  };
 
   return (
     <Layout>
@@ -108,34 +81,8 @@ const App = () => {
               background: colorBgContainer,
             }}
           >
-            {/* 走马灯 */}
-            <Carousel afterChange={onChange}>
-      <div>
-        <h3 style={contentStyle}>1</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>2</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>3</h3>
-      </div>
-      <div>
-        <h3 style={contentStyle}>4</h3>
-      </div>
-    </Carousel>
-
-    {/* 折叠面板 */}
-    <Collapse defaultActiveKey={['1']} onChange1={onChange1}>
-      <Panel header="This is panel header 1" key="1">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 2" key="2">
-        <p>{text}</p>
-      </Panel>
-      <Panel header="This is panel header 3" key="3">
-        <p>{text}</p>
-      </Panel>
-    </Collapse>
+            <MyFirstComponent propsName={"我是组件一来自props的值"}></MyFirstComponent>
+            <MyTwoComponent propsName={"我是来自组件二的props的值"}></MyTwoComponent>
           </Content>
         </Layout>
       </Layout>
